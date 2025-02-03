b7b2242d82fb631ce5549e152616dfaaaf161154
moekak <98260675+moekak@users.noreply.github.com>
2025-02-03 12:32:36 +0900

    リファクタリング

--- js/error.js
+++ js/error.js
@@ -1,103 +1,53 @@
 
 const submit = document.getElementById("js_button");
-const firstName = document.getElementById("first_name");
-const firstNameInput = document.querySelector(".first_name");
-const lastName = document.getElementById("last_name");
-const lastNameInput = document.querySelector(".last_name");
-const email = document.getElementById("email");
-const emailInput = document.querySelector(".email");
-const message = document.getElementById("message");
-const messageInput = document.querySelector(".message");
-const checkbox = document.getElementById("js_checkbox");
-const errorText1 = document.querySelector(".js_error-text1");
-const errorText2 = document.querySelector(".js_error-text2");
-const errorText3 = document.querySelector(".js_error-text3");
-const errorText4 = document.querySelector(".js_error-text4");
-const errorText5 = document.querySelector(".js_error-text5");
-const errorText6 = document.querySelector(".js_error-text6");
-
 const sendMessage = document.querySelector(".send_message");
-
-//ダメなの分かっていますが、後から変更する前提でerrorText1など数字でナンバリングしてます。理解＆整理しやすいので最初だけお許しを
-
-
+const errorTexts = document.querySelectorAll(".js_error-text");
 
 submit.addEventListener("click",()=>{
-    let isValid = true;
 
-    if (firstName.value.trim() ==="") {
-        displayError();
-        isValid = false;
-    } else {
-        firstNameInput.classList.remove("error-border");
-        errorText1.classList.remove("block");
-    }
-    
-    if(lastName.value.trim() ==="") {
-        displayError2();
-        isValid = false;
-    } else {
-        lastNameInput.classList.remove("error-border");
-        errorText2.classList.remove("block");
-    }
-    if(email.value.trim() ==="") {
-        displayError3();
-        isValid = false;
-    } else {
-        emailInput.classList.remove("error-border");
-        errorText3.classList.remove("block");
-        email.placeholder = "";
-    }
-    const radio = document.querySelector('input[name="query-type"]:checked');
-    if(!radio) {
-        displayError4();
-        isValid = false;
-    } else {
-        errorText4.classList.remove("block");
-    }
-    
-    if(message.value.trim() ==="") {
-        displayError5();
-        isValid = false;
-    } else {
-        messageInput.classList.remove("error-border");
-        errorText5.classList.remove("block");
-    }
-    if(!checkbox.checked) {
-        displayError6();
-        isValid = false;
-    } else {
-        errorText6.classList.remove("block");
-    }
+    const firstName = document.getElementById("first_name");
+    const lastName = document.getElementById("last_name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    const checkbox = document.getElementById("js_checkbox");
+    const radio = document.querySelector('input[name="query-type"]:checked')
+    const inputFields = [firstName, lastName, email, radio, message, checkbox]
 
+
+    inputFields.forEach((input, index)=>{
+        const hasError = (input == null || (input.getAttribute("data-input-type") == "check" && !input.checked)) ? true : input.value.trim() === ""
+        if(hasError){
+            displayError(errorTexts[index], input)
+        }else{
+            hideError(errorTexts[index], input)
+        }
     
-    if (isValid===true){
-        console.log(isValid);
-        sendMessage.classList.add("block");// ←これが動かないのが分かりませんでした。
-    }
+    })
+
 });
 
-const displayError = ()=>{
-    firstNameInput.classList.add("error-border");
-    // errorText.classList.remove("hidden")
-    errorText1.classList.add("block");
-};
-const displayError2 = ()=>{
-    lastNameInput.classList.add("error-border");
-    errorText2.classList.add("block");
-};
-const displayError3 = ()=>{
-    emailInput.classList.add("error-border");
-    errorText3.classList.add("block");
-    email.placeholder = "email #example.com";
-};
-const displayError4 = ()=>{
-    errorText4.classList.add("block");
-};
-const displayError5 = ()=>{
-    messageInput.classList.add("error-border");
-    errorText5.classList.add("block");
-};
-const displayError6 = ()=>{
-    errorText6.classList.add("block");
-};
+
+
+// 関数う
+const isEmail = (input) =>{
+    return input !== null && input.id === "email" ? true : false
+}
+const ischeck= (input) =>{ 
+    return (input == null || input.getAttribute("data-input-type") == "check")  ? true: false
+}
+
+const displayError = (errorTxt, input)=>{
+    errorTxt.classList.add("block");
+
+    if(!ischeck(input)) {
+        input.classList.add("error-border");
+    }
+    if(isEmail(input)) input.placeholder = "email #example.com"
+}
+
+const hideError = (errorTxt, input) =>{
+    errorTxt.classList.remove("block");
+
+    if(!ischeck(input)) input.classList.remove("error-border");
+    if(isEmail(input)) input.placeholder = ""
+}
